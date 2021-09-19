55ec442a6a94995822b76cb33f319824ededf1ce
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-19 18:51:20 +0300

    clean some code issues

--- SwiftQuiz/ViewControllers/ResultViewController.swift
+++ SwiftQuiz/ViewControllers/ResultViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 enum PictureType: String {
-    case badscore
+    case badscore = "SadJpeg"
     case averagescore
-    case pefectscore
+    case perfectscore
 }
 
+enum ResultGag: String {
+    case badscore = "чему я тебя учил?!"
+    case averagescore = "неплохо, но ты можешь лучше!"
+    case perfectscore = "молодец, так держать!"
+}
 
 class ResultViewController: UIViewController {
     
@@ -31,7 +36,7 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         updateResult()
         updateUI()
-   
+        
     }
     // MARK: - Private methods
     private func updateResult () {
@@ -42,19 +47,27 @@ class ResultViewController: UIViewController {
             }
         }
     }
-        private func updateUI() {
-            switch correctAnswerCount {
-            case 0...5:
-                resultPic.image = UIImage(named: "SadJpeg")
-            case 5...10:
-                resultPic.image = UIImage(named: "NormalJpeg")
-            default:
-                resultPic.image = UIImage(named: "HappyJpeg")
-            }
-            
-            let defaults = UserDefaults.standard
-            let userName = defaults.string(forKey: "userName") ?? ""
-            resultTextLabel.text = "\(userName), вы ответили правильно на \(correctAnswerCount) из \(answers.count)"
-            
+    
+    private func updateUI() {
+        switch correctAnswerCount {
+        case 0...5:
+            resultPic.image = UIImage(named: "SadJpeg")
+        case 5...14:
+            resultPic.image = UIImage(named: "NormalJpeg")
+        default:
+            resultPic.image = UIImage(named: "HappyJpeg")
+        }
+        
+        let defaults = UserDefaults.standard
+        let userName = defaults.string(forKey: "userName") ?? ""
+        
+        switch correctAnswerCount {
+        case 0...5:
+            resultTextLabel.text = "\(userName), \(ResultGag.badscore.rawValue) ты ответил правильно всего на \(correctAnswerCount) из \(answers.count) "
+        case 5...14:
+            resultTextLabel.text = "Твой результат, \(userName), \(correctAnswerCount) из \(answers.count), \(ResultGag.averagescore.rawValue)"
+        default:
+            resultTextLabel.text = "\(userName), \(ResultGag.perfectscore.rawValue) \(correctAnswerCount) из \(answers.count)"
         }
     }
+}
