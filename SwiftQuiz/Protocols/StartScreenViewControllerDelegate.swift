8d38961da234f0865ed564d10b01f2ff4b9d1ce0
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-19 21:03:34 +0300

    add protocol

---
+++ SwiftQuiz/Protocols/StartScreenViewControllerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  StartScreenViewControllerDelegate.swift
+//  SwiftQuiz
+//
+//  Created by Повелитель on 19.09.2021.
+//
+
+import Foundation
+
+protocol StartScreenViewControllerDelegate {
+    func getUserName(name: String) 
+}
